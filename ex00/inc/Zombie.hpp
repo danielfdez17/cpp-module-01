3d9579c0d3632e8c663dc7ae570be4365e06ad66
danielfdez17 <daniel17dev@gmail.com>
2025-11-28 06:17:36 +0100

    fix: remove colors.hpp

--- ex00/inc/Zombie.hpp
+++ ex00/inc/Zombie.hpp
@@ -2,8 +2,16 @@
 #ifndef __ZOMBIE_H__
 #define __ZOMBIE_H__
 
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
 #include <string>
-#include "colors.hpp"
 
 class Zombie
 {
