bf07529a7efc9666a9f7843f58baa740bc383c8e
danielfdez17 <daniel17dev@gmail.com>
2025-11-29 21:24:41 +0100

    feat: ex04 done

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -4,14 +4,61 @@
 #include <string>
 // #include <bits/stdc++.h>
 
+void	replace(std::string &content, const std::string s1, const std::string s2)
+{
+	// ? search s1
+	int	content_size	= content.size();
+	int	s1_size			= s1.size();
+	int	s2_size			= s2.size();
+
+	// * till content_size - s1_size to avoid getting out of range
+	// ? replace s1 with s2
+	for (int i = 0; i <= content_size - s1_size; ++i)
+	{
+		int j = 0;
+		// * search s1 in content
+		for (j = 0; j < s1_size; ++j)
+			if (content[i + j] != s1[j])
+				break;
+		if (j == s1_size)
+		{
+			// ? write the content in other file
+			int replace_pos = i;
+			j = 0;
+			if (s2_size > s1_size)
+			{
+				content.resize(content_size + (s2_size - s1_size));
+				content_size = content.size();
+				// * move the content to the right to make space for s2
+				for (int k = content_size - 1; k >= replace_pos + s2_size; --k)
+					content[k] = content[k - (s2_size - s1_size)];
+			}
+			else if (s2_size < s1_size)
+			{
+				content.resize(content_size - (s1_size - s2_size));
+				content_size = content.size();
+				// * move the content to the left to fill the gap left by s2
+				for (int k = replace_pos + s2_size; k < content_size; ++k)
+					content[k] = content[k + (s1_size - s2_size)];
+			}
+			// * copy s2 to the position of s1
+			while (j < s2_size)
+				content[replace_pos++] = s2[j++];
+		}
+	}
+}
+
 int main(int ac, char **av)
 {
 	if (ac != 4)
+	{
+		std::cerr << "Usage: " << av[0] << " <filename> <s1> <s2>\n";
 		return 1;
+	}
 	std::string	filename	= av[1];
 	std::string	s1			= av[2];
 	std::string	s2			= av[3];
-	std::string content;
+	std::string content		= "";
 
 	// ? open file
 	std::fstream file;
@@ -21,46 +68,19 @@ int main(int ac, char **av)
 		std::cerr << "Error when opening the file " << filename << "\n";
 		return 1;
 	}
+
 	// ? read the content
 	std::string line;
 	while (std::getline(file, line))
 		content += line + "\n";
-	// ? search s1
-	size_t content_size = content.size();
-	size_t s1_size = s1.size();
-	for (size_t i = 0; i <= content_size - s1_size; ++i)
-	{
-		size_t j;
-		for (j = 0; j < s1_size; ++j)
-			if (content[i + j] != s1[j])
-				break;
-		// ? replace s1 with s2
-		if (j == s1_size)
-		{
-			// ? write the content in other file
-			int replace_pos = i;
-			j = 0;
-			size_t s2_size = s2.size();
-			if (s2_size > s1_size)
-			{
-				content.resize(content_size + (s2_size - s1_size));
-				content_size = content.size();
-				for (size_t k = content_size - 1; k >= replace_pos + s2_size; --k)
-					content[k] = content[k - (s2_size - s1_size)];
-			}
-			else if (s2_size < s1_size)
-			{
-				content.resize(content_size - (s1_size - s2_size));
-				content_size = content.size();
-				for (size_t k = replace_pos + s2_size; k < content_size; ++k)
-					content[k] = content[k + (s1_size - s2_size)];
-			}
-			while (j < s2_size)
-				content[replace_pos++] = s2[j++];
-		}
-	}
+
 	// ? close the file
 	file.close();
+
+	// ? replace s1 with s2 in content
+	replace(content, s1, s2);
+
+	// ? write the content to a new file
 	std::fstream outfile;
 	outfile.open(& (filename + ".replace")[0], std::fstream::out);
 	if (!outfile.is_open())
@@ -68,7 +88,9 @@ int main(int ac, char **av)
 		std::cerr << "Error when opening the file " << filename + ".replace" << "\n";
 		return 1;
 	}
+	// ? write the content to the new file
 	outfile << content;
+	// ? close the new file
 	outfile.close();
 	return 0;
 }
