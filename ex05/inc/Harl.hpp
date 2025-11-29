ce182d2c26141faaa8fa4c255defa472c07338d4
danielfdez17 <daniel17dev@gmail.com>
2025-11-29 21:54:05 +0100

    feat: ex05 done

---
+++ ex05/inc/Harl.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef __HARL__
+#define __HARL__
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
+#include <string>
+
+typedef struct s_pair
+{
+	std::string	key;
+	void		(*value)(void);
+} t_pair;
+
+typedef short unsigned int sui;
+
+class Harl
+{
+private:
+	static const sui	MAX_OPS = 4;
+	t_pair				map[MAX_OPS];
+
+	static void			debug(void);
+	static void			info(void);
+	static void			warning(void);
+	static void			error(void);
+
+public:
+	Harl(void);
+	void		complain(std::string level);
+
+};
+
+#endif // __HARL__
---
+++ ex05/src/Harl.cpp
@@ -0,0 +1,49 @@
+#include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl(void)
+{
+	map[0].key = "debug";
+	map[0].value = &Harl::debug;
+	map[1].key = "info";
+	map[1].value = &Harl::info;
+	map[2].key = "warning";
+	map[2].value = &Harl::warning;
+	map[3].key = "error";
+	map[3].value = &Harl::error;
+}
+
+void	Harl::debug(void)
+{
+	std::cout	<< CYAN ""
+				<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n"
+				<< RESET;
+}
+
+void	Harl::info(void)
+{
+	std::cout	<< GREEN ""
+				<< "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n"
+				<< RESET;
+}
+
+void	Harl::warning(void)
+{
+	std::cout	<< YELLOW ""
+				<< "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n"
+				<< RESET;
+}
+
+void	Harl::error(void)
+{
+	std::cout	<< RED ""
+				<< "This is unacceptable! I want to speak to the manager now.\n"
+				<< RESET;
+}
+
+void		Harl::complain(std::string level)
+{
+	for (sui i = 0; i < MAX_OPS; ++i)
+		if (level == map[i].key)
+			map[i].value();
+}
